212083f478a7a56f38e72d3e23adfbdb1bb825b4
isdance <isdance2004@hotmail.com>
2019-04-13 15:57:07 +1000

    add Modal using React portals

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Modal from './Modal'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleShowMessageClick = () => setShowModal(true)
+  const handleCloseModal = () => setShowModal(false)
+
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'grid',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <div
+        style={{
+          maxWidth: 400,
+          position: 'relative'
+        }}
+      >
+        <h1>My App</h1>
+        <p>
+          This is an example of how you might use React.createPortal. I think it
+          is a pretty neat API that is yet another awesome escape hatch that
+          React provides for practical reasons. Sometimes you just need to
+          render something completely outside the React Tree.
+        </p>
+        <button onClick={handleShowMessageClick}>Show Secret Modal</button>
+        {showModal ? (
+          <Modal onClose={handleCloseModal}>
+            This is the secret modal message!
+          </Modal>
+        ) : null}
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default App;
+export default App
